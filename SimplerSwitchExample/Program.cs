5821cf0f0ab139288254a29dd26a7bb60fe7749c
Sławomir Kowalski <sawekk982@gmail.com>
2018-07-14 12:42:20 +0100

    Update Program.cs

--- SimplerSwitchExample/Program.cs
+++ SimplerSwitchExample/Program.cs
@@ -29,32 +29,4 @@ namespace SwtchSimplerExample
             Console.ReadKey();
         }
     }
-
-    public class Light
-    {
-        bool LightState = false;
-
-        public void PowerOn()
-        {
-            CheckState(true, "The light is already turned on!", "The light is on");
-        }
-
-        public void PowerOff()
-        {
-            CheckState(false, "The light is already turned off!", "The light is off");
-        }
-
-        public void CheckState(bool state, string execption, string notification)
-        {
-            if (LightState == state)
-            {
-                Console.WriteLine(execption);
-            }
-            else
-            {
-                Console.WriteLine(notification);
-                LightState = state;
-            }
-        }
-    }
 }
