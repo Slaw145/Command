3b4b5ca11f72e36309ed63048947251cef17a6c3
Sławomir Kowalski <sawekk982@gmail.com>
2018-07-14 12:43:07 +0100

    Update Light.cs

--- SimplerSwitchExample/Light.cs
+++ SimplerSwitchExample/Light.cs
@@ -4,5 +4,31 @@ using System.Text;
 
 namespace SwtchSimplerExample
 {
+      public class Light
+      {
+          bool LightState = false;
 
+          public void PowerOn()
+          {
+              CheckState(true, "The light is already turned on!", "The light is on");
+          }
+
+          public void PowerOff()
+          {
+              CheckState(false, "The light is already turned off!", "The light is off");
+          }
+
+          public void CheckState(bool state, string execption, string notification)
+          {
+              if (LightState == state)
+              {
+                  Console.WriteLine(execption);
+              }
+              else
+              {
+                  Console.WriteLine(notification);
+                  LightState = state;
+              }
+          }
+      }
 }
